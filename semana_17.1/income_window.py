89c2035e4a1594ee56d99a56f60c4fa00af25a11
FreCalvo <frecalvo90@gmail.com>
2024-10-26 15:10:13 -0600

    Initial submission Final Project Python Module

---
+++ semana_17.1/income_window.py
@@ -0,0 +1,47 @@
+import PySimpleGUI as sg
+import out_functions as l
+import data_import_export as d
+
+
+def add_income(data_array, file_name, list_categories):
+    add_income_layout = [[sg.Text("Detail:"), sg.Input(key='-DETAIL-', do_not_clear=True, size=(20, 1)), sg.Text(key='-ERROR_1-')],
+        [sg.Text("Amount:"), sg.Input(key='-AMOUNT-', do_not_clear=True, size=(10, 1)), sg.Text(key='-ERROR_2-')],
+        [sg.Text("Category:"), sg.Input(key='-CATEGORY-', do_not_clear=True, size=(10, 1)), sg.Text(key='-ERROR_3-')],
+        [sg.Text("")],
+        [sg.Push(), sg.Button('Save'), sg.Button('Back'), sg.Push()]]
+
+    add_income_window = sg.Window("Add Income", add_income_layout)
+
+    while True:
+        event, values = add_income_window.read()
+        if event in (sg.WIN_CLOSED, 'Back'):
+            break
+        elif event == 'Save':
+            add_income_window['-ERROR_1-'].Update('')
+            add_income_window['-ERROR_2-'].Update('')
+            add_income_window['-ERROR_3-'].Update('')
+            print(list_categories)
+            
+            detail_check = l.check_is_not_blank(values['-DETAIL-'])
+            if detail_check == True:
+                add_income_window['-ERROR_1-'].Update(f'Error: Space cannot be left blank!')
+
+            amount_check = l.check_for_number_in_input(values['-AMOUNT-'])
+            if amount_check == False:
+                add_income_window['-ERROR_2-'].Update(f'Error: Enter numbers!')
+
+            category_check = l.check_if_category_in_list(list_categories, values['-CATEGORY-'])
+            if category_check == False:
+                add_income_window['-ERROR_3-'].Update(f'Error: Category is not listed!')
+
+            if detail_check == False and amount_check == True and category_check == True:
+                l.store_information_in_data_list('Income', values['-DETAIL-'], values['-AMOUNT-'], values['-CATEGORY-'], data_array)
+
+                sg.popup("Information saved!!")
+                d.export_data(data_array,file_name)
+
+
+                add_income_window['-DETAIL-'].Update('')
+                add_income_window['-AMOUNT-'].Update('')
+                add_income_window['-CATEGORY-'].Update('')
+    add_income_window.close()
---
+++ semana_17.1/main_window.py
@@ -0,0 +1,60 @@
+import PySimpleGUI as sg
+import expense_window as e
+import income_window as i
+import category_window as c
+import out_functions as l
+
+
+def main_window():
+    file_data = 'data.csv'
+    data_array = []
+    # d.import_data(file_data, data_array)
+    file_categories = 'categories.csv'
+    list_categories = []
+
+    l.update_content(file_data, data_array)
+    l.update_content(file_categories, list_categories)
+
+    headings = ["Type","Detail", "Amount","Category"]
+    
+    sg.theme("LightBrown1")
+
+    main_window_layout = [
+        [sg.Push(),sg.Button('Add Category'), sg.Button('Add Expense'), sg.Button('Add Income'),sg.Push()],
+        [sg.HorizontalSeparator(color='white')],
+        [sg.Text()],
+        [sg.Table(values=data_array, headings=headings, max_col_width=35,
+            auto_size_columns=True,
+            display_row_numbers=True,
+            justification='center',
+            num_rows=10,
+            enable_events=True,
+            key='-INFORMATION_TABLE-',
+            row_height=15,
+            tooltip='Data Table')],
+            [sg.Push(), sg.Button('Update table'), sg.Text(key='-ERROR_1-'), sg.Push()],
+    ]
+
+    main_window = sg.Window("Finance tracker", main_window_layout)
+
+    while True:
+        event, values = main_window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == 'Add Category':
+            c.add_category(list_categories, file_categories)
+        elif event == 'Add Expense':
+            main_window['-ERROR_1-'].Update(f'')
+            e.add_expense(data_array, file_data, list_categories)
+        elif event == 'Add Income':
+            main_window['-ERROR_1-'].Update(f'')
+            i.add_income(data_array, file_data, list_categories)
+        elif event == 'Update table':
+            if data_array == []:
+                main_window['-ERROR_1-'].Update(f'Data file is empty. Proceed to add expense or income')
+
+            else:
+                print(data_array)
+                main_window['-INFORMATION_TABLE-'].Update(values=data_array)
+
+main_window()
